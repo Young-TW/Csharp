e5ca9b085fe19d48f4c2ad8f2ccd7bc0bb659fae
Young-TW <young20050727@gmail.com>
2021-09-03 00:23:13 +0800

    晚安

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace Csharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Beep();
+            int[] a = new int[5] {1,2,3,4,5};
+            for(int i=0;i<5;i++){
+                Console.WriteLine(a[i]);
+            }
         }
     }
 }
